cc8f5104eda7432375610273c7f17555a0495526
Jonathan Yiv <JonathanYiv@gmail.com>
2017-09-30 01:23:19 -0400

    extracted ChessText into a separate file

--- lib/game.rb
+++ lib/game.rb
@@ -6,6 +6,7 @@ require_relative "bishop.rb"
 require_relative "king.rb"
 require_relative "pawn.rb"
 require_relative "string.rb"
+require_relative "chesstext.rb"
 require "pry"
 require "yaml"
 
@@ -362,35 +363,5 @@ class Game
 			end
 		end
 		false
-	end
-end
-
-class ChessText
-		def ChessText.title
-		print " .d8888b.  888                                 
-d88P  Y88b 888                                 
-888    888 888                                 
-888        88888b.   .d88b.  .d8888b  .d8888b  
-888        888 \"88b d8P  Y8b 88K      88K      
-888    888 888  888 88888888 \"Y8888b. \"Y8888b. 
-Y88b  d88P 888  888 Y8b.          X88      X88 
- \"Y8888P\"  888  888  \"Y8888   88888P\'  88888P\' \n\n"
-	end
-
-	def ChessText.instructions
-		print "Instructions at https://www.chess.com/learn-how-to-play-chess\n\n"
-	end
-
-	def ChessText.win(player)
-		puts "Congratulations, #{player.name.bold}! You are the Champion!"
-	end
-
-	def ChessText.promotion_prompt
-		print "\nYour Pawn has reached the end of the board, #{turn_player.name.bold}!\n\n"
-		print "You can promote your Pawn into a Queen, Knight, Rook, or Bishop. Please input which:\n> "
-	end
-
-	def ChessText.invalid_promotion
-		print "\n That doesn't appear to be something you can promote a pawn to.. Try again:\n> "
 	end
 end
